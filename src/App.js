7258f140fb49fae2b2597d40a204f92041f8f8fd
dvkeia <devikak475@gmail.com>
2023-03-30 16:46:07 -0400

    3/30 experiences page

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import Footer from './components/Footer';
 
 function App() {
   return (
-    <div>
+    <div className='App'>
       <Router>
       <Navbar/> 
         <Routes>
--- src/components/pages/Experience.js
+++ src/components/pages/Experience.js
@@ -1,8 +1,40 @@
 import React from 'react'
+import {
+  VerticalTimeline,
+  VerticalTimelineElement,
+} from "react-vertical-timeline-component";
+import "react-vertical-timeline-component/style.min.css";
+import SchoolIcon from '@mui/icons-material/School'; //mui.com for import statements
+import WorkIcon from '@mui/icons-material/Work';
 
-const Experience = () => {
+//vertical tiemline website has documentation (google for it to adjust) stephane-monnot?
+const Experience = () => { //classNames within headers help it match with correct CSS style
   return (
-    <div>Experience</div>
+    <div>
+      <VerticalTimeline lineColor='#3e497a'>
+        <VerticalTimelineElement //within the element brackets, set style/appearance, within Element brackets, put content with corr. html tags
+          className="vertical-timeline-element--education"
+          date= "August 2022 - Expected Graduation 2026"
+          icon = {<SchoolIcon/>}
+          iconStyle={{background: "#8198FD", color: "#fff"}}
+        >
+          <h3 className='vertical-timeline-element-title'>University of Florida</h3> 
+          <h4 className='vertical-timeline-element-subtitle'>Bachelor's Degree</h4>
+          <p>Computer Engineering</p>
+        </VerticalTimelineElement>
+
+        <VerticalTimelineElement //within the element brackets, set style/appearance, within Element brackets, put content with corr. html tags
+          className="vertical-timeline-element--work"
+          date= "August 2022 - November 2022"
+          icon = {<WorkIcon/>}
+          iconStyle={{background: "#8198FD", color: "#fff"}}
+        >
+          <h3 className='vertical-timeline-element-title'>Coding Tutor</h3> 
+          <h4 className='vertical-timeline-element-subtitle'>Women in Computer Science</h4>
+          <p>Tutored local high school girls in basics of coding and mentored them to pursue coding</p>
+        </VerticalTimelineElement>
+      </VerticalTimeline>
+    </div>
   )
 }
 
